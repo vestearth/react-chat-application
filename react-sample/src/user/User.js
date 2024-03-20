244f05ef7549fb7fc0e882839b2b893c9b03763c
ESK <sichol.kor@gmail.com>
2024-03-21 06:34:32 +0700

    Upload Test app to git

---
+++ react-sample/src/user/User.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+import withRequest from "../lib/withRequest";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Email</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data === undefined ? (
+              <div />
+            ) : (
+              this.props.data.map(user => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default User;
